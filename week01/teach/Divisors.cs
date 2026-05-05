441044910e567c4428fef6f31693cff72c912e22
mrmerarif <bflores2@byupathway.edu>
2026-05-05 00:34:19 -0600

    week 1 activities

--- week01/teach/Divisors.cs
+++ week01/teach/Divisors.cs
@@ -1,12 +1,15 @@
-public static class Divisors {
+public static class Divisors
+{
     /// <summary>
     /// Entry point for the Divisors class
     /// </summary>
-    public static void Run() {
+    public static void Run()
+    {
         List<int> list = FindDivisors(80);
         Console.WriteLine("<List>{" + string.Join(", ", list) + "}"); // <List>{1, 2, 4, 5, 8, 10, 16, 20, 40}
         List<int> list1 = FindDivisors(79);
         Console.WriteLine("<List>{" + string.Join(", ", list1) + "}"); // <List>{1}
+
     }
 
     /// <summary>
@@ -16,8 +19,17 @@ public static class Divisors {
     /// </summary>
     /// <param name="number">The number to find the divisor</param>
     /// <returns>List of divisors</returns>
-    private static List<int> FindDivisors(int number) {
-        List<int> results = new();
+    private static List<int> FindDivisors(int number)
+    {
+        List<int> results = new List<int>();
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                results.Add(i);
+            }
+        }
+
         // TODO problem 1
         return results;
     }
